cc4ada0200d49da132006368880fefcbf3393b52
Premsai J S <premsai12207@gmail.com>
2026-07-08 20:44:47 +0530

    Time: 2 ms (92.17%), Space: 47.9 MB (38.19%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int start = 0;
+        for(int i = 0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[start]=nums[i];
+                start++;
+            }
+        }    
+        while(start<nums.length)
+        {
+            nums[start++]=0;
+        }
+    }
+}
