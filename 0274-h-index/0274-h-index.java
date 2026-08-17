e8d2114a7931d25b0938c089923e9c78e5234666
Premsai J S <premsai12207@gmail.com>
2026-08-17 21:22:12 +0530

    Time: 7 ms (9.68%), Space: 43.8 MB (15.49%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int h = 0;
+
+        for (int i = 0; i < n; i++) {
+            h = Math.max(h, Math.min(citations[i], n - i));
+        }
+
+        return h;
+    }
+}
